e67a33d1e526810ed35ee166d208a2ee75c827e4
Fanboynz <mp3geek@gmail.com>
2026-09-05 19:43:06 +1200

    build: staging 438 / iOS 319 for the next testing build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         applicationId = "com.noop.whoop"
         minSdk = 26
         targetSdk = 34
-        versionCode = 437
+        versionCode = 438
         versionName = "11.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
