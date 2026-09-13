03d4a2f609c8fa6819d3a49c22aa1634c3bc33d0
Fanboynz <mp3geek@gmail.com>
2026-09-14 00:16:58 +1200

    build: testing build 501 / 382

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         applicationId = "com.noop.whoop"
         minSdk = 26
         targetSdk = 34
-        versionCode = 500
+        versionCode = 501
         versionName = "11.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
